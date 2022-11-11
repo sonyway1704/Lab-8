031b5f514410869e67d8ec37009d479afb611ff5
sonyway <baotoantrinh2002@gmail.com>
2022-11-11 21:49:08 +0700

    Initial commit

---
+++ lab8.java
@@ -0,0 +1,13 @@
+public class lab8{
+    public static int sum(int n){
+        if(n==1){
+            return 1;
+        }
+        else{
+            return n+sum(n-1);
+        }
+    }
+    public static void main(String[] args){
+        System.out.println(sum(5));
+    }
+}
